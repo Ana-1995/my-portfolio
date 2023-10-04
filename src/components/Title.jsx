034cd9eeb8a937041dada56bc5e56056ecee6bbf
Anano <ananoiashagashvili@gmail.com>
2023-10-04 15:49:25 +0400

    updated

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Title = ({ title }) => {
   return (
-    <h1 className='text-center name py-3 lg:py-5 capitalize text-5xl md:text-6xl tracking-wider font-semibold'>
+    <h1 className='text-center name py-3 lg:py-5 capitalize text-5xl md:text-6xl tracking-wider font-bold'>
       <span className='bg-gradient-to-r from-[var(--title1-gradient-from)] to-[var(--title1-gradient-to)] bg-clip-text text-transparent font-bold'>
         {title}
       </span>
